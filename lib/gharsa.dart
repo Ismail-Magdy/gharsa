f31c1983bccfc5fd542036645aaa5825d0662e58
Ismail Magdy <ismailmagdy920@gmail.com>
2026-09-23 19:44:42 +0300

    Refactor app structure: replace MyApp with Gharsa and update widget tests accordingly

---
+++ lib/gharsa.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Gharsa extends StatelessWidget {
+  const Gharsa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
